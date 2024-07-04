d0dc919dbe781a6f3497499004dc5bf039a654de
Alisher Goyibov <alishergoyibov76@gmail.com>
2024-07-04 17:05:29 +0500

    commit

--- src/main/java/pdp/uz/migration/entity/UserEntity.java
+++ src/main/java/pdp/uz/migration/entity/UserEntity.java
@@ -1,5 +1,21 @@
 package pdp.uz.migration.entity;
 
-public class UserEntity {
+import jakarta.persistence.*;
+import lombok.*;
 
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+//    private String name;
+    private String mail;
+    private String username;
+//    private String password;
 }
--- src/main/java/pdp/uz/migration/repo/UserEntityRepository.java
+++ src/main/java/pdp/uz/migration/repo/UserEntityRepository.java
@@ -1,4 +1,12 @@
-package pdp.uz.migration.repo;
-
-public interface UserEntityRepository extends org.springframework.data.jpa.repository.JpaRepository<pdp.uz.migration.entity.UserEntity, java.lang.Long> {
-  }
+//package pdp.uz.migration.repo;
+//
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import pdp.uz.migration.entity.UserEntity;
+//
+//import java.util.List;
+//import java.util.Optional;
+//
+//public interface UserEntityRepository extends JpaRepository<UserEntity, Long> {
+//    Optional<UserEntity> findById(String email);
+//    List<UserEntity> findAll();
+//}
